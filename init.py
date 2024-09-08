0a6efa9d404d8972295859672ed23689eac0556d
Danilo Djokic <djokicd@outlook.com>
2024-09-08 17:50:17 +0200

    Допуна init.py

--- init.py
+++ init.py
@@ -11,6 +11,7 @@ def main(rok, godina, datum, k = False, i = False, opcije = True):
         os.rename(glob.glob('si1oe_*.tex')[0], f'si1oe_{rok}-{godina}.tex') # Rename si1oe rok
         
         with open("generics.tex", 'w', encoding='utf-8') as generics:
+            generics.write(r"\newboolean{opcijeZaPolaganje}" + "\n")
             generics.write(r"\newcommand{\datumIspita}{" + datum + " г}" + "\n")
             
             # U ispitnim rokovima posle februara, ne treba nuditi opcije za polaganje ispita
@@ -20,10 +21,14 @@ def main(rok, godina, datum, k = False, i = False, opcije = True):
             else:
                 generics.write(r"\setboolean{opcijeZaPolaganje}{false}" + "\n")
 
+            generics.write(r"\newboolean{ispit}" + "\n" )
+
             if i:
+                generics.write(r"\setboolean{ispit}{true}" + "\n")
                 generics.write(r"\newcommand{\naslovFormulara}{ИСПИТ ИЗ ОСНОВА ЕЛЕКТРОНИКЕ}" + "\n") 
                 generics.write(r"\newcommand{\tablicaPDF}{si1oe_tablica_ispit.pdf}" + "\n") 
             else:
+                generics.write(r"\setboolean{ispit}{false}")
                 generics.write(r"\newcommand{\tablicaPDF}{si1oe_tablica_kolokvijum.pdf}" + "\n") 
                 if k == 1:
                     generics.write(r"\newcommand{\naslovFormulara}{ПРВИ КОЛОКВИЈУМ ИЗ ОСНОВА ЕЛЕКТРОНИКЕ}" + "\n")    
